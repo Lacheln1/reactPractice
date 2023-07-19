17d0097511067c1e8caaab49b544aad68dc76859
Lacheln1 <59949555+Lacheln1@users.noreply.github.com>
2023-07-19 23:52:36 +0900

    공부2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Counter from './counter';
+
+import InputSample from './inputSample';
 
 
 function App() {
   return (
-   <Counter/>
+   <InputSample/>
   );
 }
 
---
+++ src/inputSample.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+function InputSample(){
+    const [text, setText] = useState('');
+
+    const onChange = (e) =>{
+        setText(e.target.value);
+    }
+    return (
+        <div>
+            <input onChange={onChange} value={text}/>
+            <button>초기화</button>
+            <div>
+                <b>값: </b>
+                {text}
+            </div>
+        </div>
+    )
+}
+
+export default InputSample;
