8064710899b75e5a37e2610ad1114fdaee31d53d
Lacheln1 <59949555+Lacheln1@users.noreply.github.com>
2023-07-21 22:46:23 +0900

    공부

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+
+import UserList from './UserList';
 import InputSample from './inputSample';
 
 
 function App() {
+  const users = [
+    {
+        id: 1,
+        username:'lacheln',
+        email:'psio@naver.com'
+    },
+    {
+        id:2,
+        username:'test1',
+        email: 'test@naver.com'
+    },
+    {
+        id:3,
+        username:'test2',
+        email:'test2@naver.com'
+    }
+];
   return (
-   <InputSample/>
+   <UserList users={users}/>
   );
 }
 
