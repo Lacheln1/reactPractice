db2fd6a48b2a798aa4aa97bfd966fec958a9afc9
Lacheln1 <psionict@naver.com>
2023-07-25 23:57:07 +0900

    0724-2

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({ user , onRemove ,onToggle}) {
     const {username,email,id,active}=user;
+    useEffect(()=>{
+      console.log('user값이 설정됨')
+      console.log(user);
+      return ()=>{
+        console.log('user값이 바뀌기전');
+        console.log(user);
+      }
+    },[user]);
+
   return (
     <div>
         <b style={{
